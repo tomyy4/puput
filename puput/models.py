dcc69e2bc1b0d47776eeb09adb8367955a2e5838
tllompartavoristravelcom <t.llompart@avoristravel.com>
2022-07-06 10:26:32 +0200

    #add-image-field
    fix default value

--- puput/models.py
+++ puput/models.py
@@ -63,7 +63,7 @@ class Category(models.Model):
         on_delete=models.SET_NULL
     )
     description = models.CharField(max_length=500, blank=True, verbose_name=_('Description'))
-    image_url = models.CharField(max_length=255, verbose_name=_('URL'), blank=True, default=True)
+    image_url = models.CharField(max_length=255, verbose_name=_('URL'), blank=True, null=True)
 
     objects = CategoryManager()
 
